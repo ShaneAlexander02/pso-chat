5717ca869da109cc3dc2db9c64ff0727ac563ca7
ShaneAlexander02 <shane.giorgio@gmail.com>
2024-06-05 23:11:10 +0700

    nambahin pipeline buat testing make jest

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,9 @@
+// __tests__/uselessTest.test.js
+
+// This test suite doesn't test any real functionality
+describe("buat masukin pipeline doang", () => {
+  // This test always passes
+  test("test test", () => {
+    expect(true).toBe(true);
+  });
+});
