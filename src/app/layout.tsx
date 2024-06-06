d4e04944670caf07d2ca6e0608d0492ba1f7d502
Amadeus <anantoamadeus@gmail.com>
2024-06-06 23:24:43 +0700

    simpan progress layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import { Providers } from "./providers";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers><TooltipProvider>{children}</TooltipProvider></Providers>
       </body>
     </html>
   );
