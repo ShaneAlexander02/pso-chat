7b50e3534e174e2ffb046d6b53c0a0492e444789
Amadeus <anantoamadeus@gmail.com>
2024-06-10 12:04:41 +0700

    otomatis redirect /conversations kalo udah signed in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,30 @@
 "use client";
 
-import { SignInButton, UserButton } from "@clerk/nextjs";
+import { SignInButton, UserButton, useAuth } from "@clerk/nextjs";
 import { Authenticated, Unauthenticated } from "convex/react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const { isSignedIn } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      router.push("/conversations")
+    }
+ 
+  }, [isSignedIn, router]);
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Unauthenticated>
         <SignInButton />
       </Unauthenticated>
-      <Authenticated>
-        <UserButton />
-      </Authenticated>
     </main>
   );
 }
+
+
