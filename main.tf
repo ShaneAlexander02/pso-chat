a26f9c75ad383ee0c4a6450fc8090f21eb181457
ShaneAlexander02 <shane.giorgio@gmail.com>
2024-06-13 17:12:27 +0700

    benerin instances terraform

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ variable "key_name" {
 }
 
 resource "aws_key_pair" "key_pair" {
-  key_name   = var.key_name
+  key_name   = "var.key_name"
   public_key = tls_private_key.rsa_4096.public_key_openssh
 }
 
