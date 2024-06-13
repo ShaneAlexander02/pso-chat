db6e5edac3240a51c435253ddae0f898bbe503ed
ShaneAlexander02 <shane.giorgio@gmail.com>
2024-06-13 16:43:28 +0700

    test terraform connect instance baru

--- main.tf
+++ main.tf
@@ -34,12 +34,50 @@ resource "local_file" "private_key" {
   filename = var.key_name
 }
 
+resource "aws_security_group" "allow_http_ssh" {
+  name        = "allow_http_ssh"
+  description = "Allow HTTP and SSH inbound traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "public_instance" {
   ami = "ami-080660c9757080771"
   instance_type = "t2.micro"
   key_name = aws_key_pair.key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
 
   tags = {
-    Name = "instance_terraform"
+    Name = "instance_terraform_2"
   }
+}
+
+output "instance_ip" {
+  value = aws_instance.public_instance.public_ip
 }
