5b6b8ddd4e4ecb2e6df753dbf590b06d48a6d193
ShaneAlexander02 <shane.giorgio@gmail.com>
2024-06-19 14:59:46 +0700

    push test unit baru doang

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,9 +1,47 @@
-// __tests__/uselessTest.test.js
+import { render } from "@testing-library/react";
+import ConversationPage from "@/app/(root)/conversations/[conversationid]/page";
+import Providers from "@/app/providers";
+import ConversationsPage from "@/app/(root)/conversations/page";
+import { useRouter } from "next/navigation";
+import Home from "@/app/page";
+import { fireEvent } from "@testing-library/react";
 
-// This test suite doesn't test any real functionality
-describe("buat masukin pipeline doang", () => {
-  // This test always passes
-  test("test test", () => {
-    expect(true).toBe(true);
+jest.mock("convex/react", () => ({
+  ConvexReactClient: jest.fn(),
+}));
+
+jest.mock("@clerk/nextjs", () => ({
+  useAuth: jest.fn().mockReturnValue({
+    isSignedIn: true,
+    userId: "mock-user-id",
+  }),
+}));
+
+describe("ConversationPage", () => {
+  it("renders ConversationContainer with children", () => {
+    const { getByText } = render(<ConversationPage />);
+    const textElement = getByText("ConversationPage");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  describe("AppPage", () => {
+    it("redirects to /conversations when user is authenticated", async () => {
+      const mockRouter = {
+        push: jest.fn(),
+      };
+
+      jest.mock("@clerk/nextjs", () => ({
+        useAuth: jest.fn().mockReturnValue({
+          isSignedIn: true,
+          userId: "mock-user-id",
+        }),
+      }));
+
+      jest.mock("next/navigation", () => ({
+        useRouter: jest.fn().mockReturnValue(mockRouter),
+      }));
+
+      render(<ConversationsPage />);
+    });
   });
 });
